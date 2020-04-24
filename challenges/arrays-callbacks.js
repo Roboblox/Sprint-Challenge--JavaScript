80f5d7aaa776781a0f76c5613252e61775a8cc15
roboblox <supeecyborg@gmail.com>
2020-04-24 15:01:11 -0400

    workingstil

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -69,19 +69,29 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 */
 const displayNames = [];
+zooAnimals.forEach(function (item) {
+  if (item === item.animal_name || item.scientific_name) {
+    displayNames.push(item);
+  }
+});
 console.log(displayNames);
 
 /* Request 2: .map()
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((uppercase) => {
+  return uppercase.animal_name.toLowerCase();
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter((pop) => {
+  return pop.population < 5;
+});
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -98,7 +108,13 @@ console.log(populationTotal);
  * The last parameter accepts a callback
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
-
+function cb(a, b) {
+  return a + b;
+}
+function consume(a, b, cb) {
+  return cb(a, b);
+}
+console.log(consume(a, b, cb(a, b)));
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers
